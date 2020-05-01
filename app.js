5d794afa5dc1dfcf2080179fe58d0f03b85a6255
Maximiano Leite <macio.pb.cg@gmail.com>
2020-05-01 18:15:34 -0300

    usando context para eviar dados

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
-'use strict'
+const NomeContext = React.createContext('nome')
 
-function meuComponente1() {
-    return (
-        React.createElement('div', {className: 'class1'},
+const meuComponente4 = () => (
+    React.createElement(NomeContext.Consumer, null, 
+        (nome) => (
+            React.createElement('section', {className: 'meuComponente3'},
+                React.createElement('p', null, `meu nome é ${nome}`)
+            )
+        )
+    )
+)
+
+const meuComponente3 = () => (
+    React.createElement('section', {className: 'meuComponente3'},
+        React.createElement(meuComponente4)
+    )
+)
+
+const meuComponente2 = () => (
+    React.createElement('section', {className: 'meuComponente2'},
+        React.createElement(meuComponente3)
+    )
+)
+
+const meuComponente1 = () => (
+    React.createElement(NomeContext.Provider, { value: 'Maximiano'},
+        React.createElement('section', {className: 'meuComponente1'},
             React.createElement(meuComponente2)
         )
     )
-}
-function meuComponente2() {
-    return (
-        React.createElement('div', {className: 'class2'},
-            React.createElement(meuComponente3)
-        )
+)
+    
+
+const meuComponente = () => (
+    React.createElement('section', {className: 'container'}, 
+        React.createElement(meuComponente1)
     )
-}
-function meuComponente3() {
-    return (
-        React.createElement('div', {className: 'class3'},
-            React.createElement(meuComponente4)
-        )
-    )
-}
-function meuComponente4() {
-    return (
-        React.createElement('p', null, 'Sou um paragrafo!')
-    )
-}
-function meuComponente() {
-    return (
-        React.createElement('div', null,
-            React.createElement(meuComponente1)
-        )
-    )
-}
+)
+    
+
 ReactDOM.render(
     React.createElement(meuComponente),
-    document.getElementById('app')
+    document.querySelector('#app')
 )
